7621255f0c37a1722b3fde51e4026b091a3cb332
Jared He <66887902+jaredjhe@users.noreply.github.com>
2021-01-16 12:52:51 -0600

    Add real/fake tweet color transition

--- src/App.js
+++ src/App.js
@@ -15,6 +15,17 @@ function App() {
     const [isUploaded, setUploaded] = useState(false);
     const [isRealTweet, setRealTweet] = useState(true);
 
+    const toggleUploaded = () => {
+        isUploaded ? setUploaded(false) : setUploaded(true);
+    }
+
+    const toggleRealTweet = () => {
+        isRealTweet ? setRealTweet(false) : setRealTweet(true);
+    }
+
+    const isReal = isUploaded && isRealTweet;
+    const isFake = isUploaded && !isRealTweet
+
     return (
         <div className="app">
             <header>
@@ -22,11 +33,19 @@ function App() {
             </header>
             <div className="main">
                 <h1>Is this Tweet fake?</h1>
-                <div className="upload-area">
+                <div className="upload-area"
+                    style={(isReal) ? { backgroundColor: "#A4FFD1", transition: "all 0.5s ease" } :
+                        ((isFake) ? { backgroundColor: "#FFC6B8", transition: "all 0.5s ease" } : 
+                        { transition: "all 0.5s ease" })}>
                     <UploadButton />
                 </div>
+                {(isReal) ? <h1>Yes, this tweet is <span style={{ color: "#00CC66" }}>real!</span></h1> : <></>}
+                {(isFake) ? <h1>Oh no, this tweet is <span style={{ color: "#FF3300" }}>fake!</span></h1> : <></>}
             </div>
             <footer>
+                {/* FOR TESTING */}
+                <button style={{ width: 200 }} onClick={toggleUploaded}>Test Uploaded</button>
+                <button style={{ width: 200 }} onClick={toggleRealTweet}>Test Real</button>
                 <ImGithub style={githubIconStyles} />
             </footer>
         </div>
