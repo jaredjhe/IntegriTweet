03e472bc6c1d5609dd4b9fa0c5e512c4ca479fbe
Jared He <66887902+jaredjhe@users.noreply.github.com>
2021-01-17 00:26:12 -0600

    Change app.js

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 const bucketName = 'test-add-tweetdetector';
-const filename = './Google Cloud/resources/wakeupcat2.jpg';
 
 // Imports the Google Cloud client library
-const {Storage} = require('@google-cloud/storage');
+const { Storage } = require('@google-cloud/storage');
 
 // Creates a client
 const storage = new Storage();
 
-async function uploadFile() {
-  // Uploads a local file to the bucket
-  await storage.bucket(bucketName).upload(filename, {
-    // Support for HTTP requests made with `Accept-Encoding: gzip`
-    gzip: true,
-    // By setting the option `destination`, you can change the name of the
-    // object you are uploading to a bucket.
-    metadata: {
-      // Enable long-lived HTTP caching headers
-      // Use only if the contents of the file will never change
-      // (If the contents will change, use cacheControl: 'no-cache')
-      cacheControl: 'public, max-age=31536000',
-    },
-  });
+async function uploadFile(filename) {
+    // Uploads a local file to the bucket
+    if (filename != "undefined") {
+        await storage.bucket(bucketName).upload(filename, {
+            // Support for HTTP requests made with `Accept-Encoding: gzip`
+            gzip: true,
+            // By setting the option `destination`, you can change the name of the
+            // object you are uploading to a bucket.
+            metadata: {
+                // Enable long-lived HTTP caching headers
+                // Use only if the contents of the file will never change
+                // (If the contents will change, use cacheControl: 'no-cache')
+                cacheControl: 'public, max-age=31536000',
+            },
+        });
+    };
 
-  console.log(`${filename} uploaded to ${bucketName}.`);
+    console.log(`${filename} uploaded to ${bucketName}.`);
 }
-
-uploadFile().catch(console.error);
-
-exports.uploadFile = uploadFile;
